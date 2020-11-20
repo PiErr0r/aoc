fb95473cb28b74be25f8b9d85af9f75fb5c4c6b7
Piero <piero@prehnit.hr>
2020-11-20 15:11:01 +0100

    + day 9 of 2018 unsolved

---
+++ 2018/09.py
@@ -0,0 +1,25 @@
+
+import math, copy, re, hashlib
+import itertools as it
+
+
+def part_1(data):
+
+	print('END OF PART1')
+	return
+
+def part_2(data):
+	print('END OF PART2')
+	return 
+
+
+if __name__ == '__main__':
+	with open('09_input') as f:
+		data = f.read()
+		data = data.split('\n')
+		data = list(map(int, data.split()))
+
+
+	part_1(copy.deepcopy(data))
+	part_2(copy.deepcopy(data))
+	
