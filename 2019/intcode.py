76fc37f1847d723a19abf6f54de91739518b5a0e
Piero <piero@prehnit.hr>
2020-11-23 23:39:01 +0100

    + added better rST comments for intcode

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,28 +1,47 @@
 class IntCode(object):
+
 	"""
-		IntCode <class> interpreter for IntCode
-
-		operation codes:
-			[int-code]: [fnc]
-			01: self.add				addition
-			02: self.mul				multiplication
-			03: self.input			get input
-			04: self.output			set output
-			05: self.jnz				jump if not zero
-			06: self.jz					jump if zero
-			07: self.lt					less than => set
-			08: self.eq					equals => set
-			09: self.rel_base		set relative address
-			99: self.halt				halt
-
-		parameters:
-			[a][b][c][opcode:2d]
-			0 - positional
-			1 - immediate 
-			2 - relative
-			
+	IntCode <class> interpreter for IntCode
+	
+	operation codes:
+		[int-code]: [fnc]
+		01: self.add				addition
+		02: self.mul				multiplication
+		03: self.input			get input
+		04: self.output			set output
+		05: self.jnz				jump if not zero
+		06: self.jz					jump if zero
+		07: self.lt					less than => set
+		08: self.eq					equals => set
+		09: self.rel_base		set relative address
+		99: self.halt				halt
+	
+	parameters:
+		[a][b][c][opcode:2d]
+		0 - positional
+		1 - immediate 
+		2 - relative
+	
+	Attributes:
+	    data (list): data/instructions of the program
+	    debug (bool): debug flag, if True display diagnostic messages
+	    func (dict): hashmap which holds the functions used in the program
+	    input_data (list): input buffer
+	    out_param (list): output buffer
+	    pause (bool): information if the program is paused
+	    pos (int): position of the instruction pointer
+	    regs (dict): registers for data which is out of bounds of the program
+	    rel_pos (int): address of the relative instruction pointer
+	
 	"""
 	def __init__(self, data, input_data = None, debug = False):
+		"""initialize the program
+		
+		Args:
+		    data (list): data/instruction of the program
+		    input_data (None, optional): input buffer
+		    debug (bool, optional): debugging flag
+		"""
 		super(IntCode, self).__init__()
 
 		self.data = data
@@ -51,20 +70,33 @@ class IntCode(object):
 		}
 
 	def is_halted(self):
-		"""Check if the program is halted"""
+		"""Check if the program is halted
+		
+		Returns:
+		    bool: if self.pos is -1 means that the program is halted
+		"""
 		return self.pos == -1
 
 	def is_paused(self):
-		"""Check if program is paused"""
+		"""Check if program is paused
+		
+		Returns:
+		    bool: True if paused
+		"""
 		return self.pause 
 
 	def unpause(self, input_data = []):
-		"""If the process is paused you can start it again and add more input data as a list"""
+		"""If the process is paused you can start it again and add more input data as a list
+		
+		Args:
+		    input_data (list, optional): You can add new input data while unpausing
+		"""
 		self.input_data += input_data
 		self.pause = False
 
 	def calculate(self):
-		"""Start the execution of the program"""
+		"""Start the execution of the program
+		"""
 		while self.pos != -1 and not self.pause:
 			param = self.data[ self.pos ]
 			opcode = f"{param%100:02d}"
@@ -74,6 +106,13 @@ class IntCode(object):
 		"""
 		get_mode <method> 
 		returns mode (positional/immediate/relative) for parameters of the function
+		
+		Args:
+		    n (int): the whole command from which the parameters mode is extracted
+		    ret_n (int): number of parameters for the instruction called
+		
+		Returns:
+		    list: modes for parameters from i + 1 to inclusively i + ret_n
 		"""
 		ns = f"{n:05d}"
 		return map(lambda x: int(x), ns[:3][::-1][:ret_n])
@@ -83,6 +122,13 @@ class IntCode(object):
 		"""
 		Return the address depending on the offset of the parameter {1, 2, 3}
 		and mode of the instruction {0, 1, 2}
+		
+		Args:
+		    mode (int): 0/1/2 depending on the mode of the instruction
+		    offset (int): which parameter of the function are we getting the address for
+		
+		Returns:
+		    int: address of the value for the parameter
 		"""
 		address = None
 		if mode == 0:
@@ -96,7 +142,12 @@ class IntCode(object):
 		return address
 
 	def set_data(self, addr, value):
-		"""Set the addr of data or registers to the value passed"""
+		"""Set the addr of data or registers to the value passed
+		
+		Args:
+		    addr (int): address which will be set
+		    value (int): value to set to the specified address
+		"""
 		if addr < 0:
 			print("FAIL - negative address")
 		if addr >= len(self.data):
@@ -105,7 +156,14 @@ class IntCode(object):
 			self.data[ addr ] = value
 
 	def get_data(self, addr):
-		"""Get value from data or registers based on address"""
+		"""Get value from data or registers based on address
+		
+		Args:
+		    addr (int): address from which we get the value
+		
+		Returns:
+		    int: value from the specified address
+		"""
 		ret_val = None
 		if addr < 0:
 			print("FAIL - negative address")
@@ -120,12 +178,24 @@ class IntCode(object):
 		return ret_val
 
 	def rel_base(self, p_addr = 0):
+		"""Change the relative address for the IntCode program
+		
+		Args:
+		    p_addr (int, optional): parameter address which hold the value to change the relative address
+		"""
 		rel_pos = self.get_address(p_addr, 1)
 		rel_val = self.get_data(rel_pos)
 		self.rel_pos += rel_val
 		self.pos += 2
 
 	def lt(self, f_addr = 0, s_addr = 0, res_addr = 0):
+		"""Set r if less than: r if f < s
+		
+		Args:
+		    f_addr (int, optional): mode for the address of the first parameter
+		    s_addr (int, optional): mode for the address of the second parameter
+		    res_addr (int, optional): mode for the address of the resulting parameter
+		"""
 		f_pos = self.get_address(f_addr,1)
 		s_pos = self.get_address(s_addr,2)
 		res_pos = self.get_address(res_addr,3)
@@ -136,6 +206,13 @@ class IntCode(object):
 
 
 	def eq(self, f_addr = 0, s_addr = 0, res_addr = 0):
+		"""Set r if equals: r if f == s
+		
+		Args:
+		    f_addr (int, optional): mode for the address of the first parameter
+		    s_addr (int, optional): mode for the address of the second parameter
+		    res_addr (int, optional): mode for the address of the resulting parameter
+		"""
 		f_pos = self.get_address(f_addr,1)
 		s_pos = self.get_address(s_addr,2)
 		res_pos = self.get_address(res_addr,3)
@@ -146,6 +223,12 @@ class IntCode(object):
 
 
 	def jnz(self, f_addr = 0, s_addr = 0):
+		"""Jump if not zero
+		
+		Args:
+		    f_addr (int, optional): if f is not zero
+		    s_addr (int, optional): jump to s
+		"""
 		f_pos = self.get_address(f_addr,1)
 		s_pos = self.get_address(s_addr,2)
 		cond = self.get_data(f_pos) != 0
@@ -157,6 +240,12 @@ class IntCode(object):
 
 
 	def jz(self, f_addr = 0, s_addr = 0):
+		"""Jump if zero
+		
+		Args:
+		    f_addr (int, optional): if f is zero
+		    s_addr (int, optional): jump to s
+		"""
 		f_pos = self.get_address(f_addr,1)
 		s_pos = self.get_address(s_addr,2)
 		cond = self.get_data(f_pos) == 0
@@ -168,6 +257,11 @@ class IntCode(object):
 
 
 	def input(self, p_addr = 0):
+		"""Get input data and set it to some address
+		
+		Args:
+		    p_addr (int, optional): address where to put the input data
+		"""
 		if len(self.input_data):
 			in_pos = self.get_address(p_addr, 1)
 			in_val = self.input_data.pop(0)
@@ -178,6 +272,11 @@ class IntCode(object):
 			self.pause = True
 
 	def output(self, p_addr = 0):
+		"""Put the data in the output buffer, if in debug mode display the data
+		
+		Args:
+		    p_addr (int, optional): address of the output data
+		"""
 		out_pos = self.get_address(p_addr, 1)
 		self.out_param += [self.get_data(out_pos)]
 		if self.debug:
@@ -185,6 +284,14 @@ class IntCode(object):
 		self.pos += 2
 
 	def get_output(self, last = 1):
+		"""Get output buffer (last `last` positions), if `last` equals -1 flush the buffer
+		
+		Args:
+		    last (int, optional): number of the data to retrieve
+		
+		Returns:
+		    list: requested output buffer
+		"""
 		if last == -1:
 			tmp = self.out_param[::]
 			self.out_param = []
@@ -192,6 +299,13 @@ class IntCode(object):
 		return self.out_param[-last:]
 
 	def mul(self, f_addr = 0, s_addr = 0, res_addr = 0):
+		"""Multiplication: res = f * r
+		
+		Args:
+		    f_addr (int, optional): address of the first parameter
+		    s_addr (int, optional): address of the second parameter
+		    res_addr (int, optional): address of the resulting parameter
+		"""
 		f_pos = self.get_address(f_addr,1)
 		s_pos = self.get_address(s_addr,2)
 		res_pos = self.get_address(res_addr,3)
@@ -205,6 +319,13 @@ class IntCode(object):
 
 
 	def add(self, f_addr = 0, s_addr = 0, res_addr = 0):
+		"""Addition: res = f + s
+		
+		Args:
+		    f_addr (int, optional): address of the first parameter
+		    s_addr (int, optional): address of the second parameter
+		    res_addr (int, optional): address of the resulting parameter
+		"""
 		f_pos = self.get_address(f_addr,1)
 		s_pos = self.get_address(s_addr,2)
 		res_pos = self.get_address(res_addr,3)
@@ -218,6 +339,8 @@ class IntCode(object):
 
 
 	def halt(self):
+		"""Halt the program
+		"""
 		print("HALT")
 		self.pos = -1
 		
