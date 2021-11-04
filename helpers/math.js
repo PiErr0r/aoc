37f51c1355eae80b5b5bce7969227f8ec65f73c8
Piero <piero.anic@gmail.com>
2021-11-04 15:38:41 +0100

    + support for single argument randint

--- helpers/math.js
+++ helpers/math.js
@@ -1,6 +1,8 @@
+const is = (n) => n !== undefined && n !== null;
 const int = (n) => Math.trunc(Number(n));
 const float = (n) => Number(n);
-const randint = (a, b) => (a + Math.floor(Math.random() * (b - a)));
+// if b is undefined or null return randint(0, a);
+const randint = (a, b) => ((is(b) ? a : 0) + Math.floor(Math.random() * ((is(b) ? b : a) - (is(b) ? a : 0))));
 
 module.exports = {
 	int,
