b0ed2c6a6b1feb4cbd50fe96af7722d6f9caf9e1
Piero <piero.anic@gmail.com>
2021-12-15 11:50:36 +0100

    + dijkstra and fastq helper

--- 2021/15.js
+++ 2021/15.js
@@ -38,35 +38,27 @@ function part1(data) {
 	return;
 }
 
-function part2(data) {
+function part2(data, part) {
 
 	let res;
 	data = lines(data).map(l => l.split('').map(a => int(a)));
+	const mul = part === 1 ? 1 : 5;
 	const f = data.length, s = data[0].length;
-	const nData = empty(5*f).map(a => empty(5*s));
-	range(f*5)(i => {
-		range(s*5)(j => {
-			if (i || j) {
-				nData[i][j] = (data[i%f][j%s]-1 + floor(i/f) + floor(j/s)) % 9 + 1;
-			}
-		})
-	})
-	nData[0][0] = 1;
-	const q = new PriorityQueue([[0,0,nData[0][0]]], (a, b) => a[2] - b[2]);
+	const fm = f * mul, sm = s * mul;
+	const getRisk = (y, x) => (data[y%f][x%s]-1 + floor(y/f) + floor(x/s)) % 9 + 1;
+	const q = new PriorityQueue([[0,0,0]], (a, b) => a[2] - b[2]);
 	const S = new set([[0,0]]);
-	let cnt = 0
+
 	while (true) {
-		// we should in place change nData values
-		// add a condition to replace the nData value if new is lower than old
-		const [x, y, risk] = q.pop();
-		if (x === 5*s - 1 && y === 5*f - 1) {
+		const [y, x, risk] = q.pop();
+		if (x === sm - 1 && y === fm - 1) {
 			res = risk ;
 			break;
 		}
 		for (const d of D4) {
 			const [dx, dy] = d;
-			if (inBB(y+dy, x+dx, nData) && !S.has([y+dy, x+dx])) {
-				q.push([y+dy, x+dx, nData[y+dy][x+dx] + risk]);
+			if (0 <= y+dy && y+dy < fm && 0 <= x+dx && x+dx < sm && !S.has([y+dy, x+dx])) {
+				q.push([y+dy, x+dx, getRisk(y+dy, x+dx) + risk]);
 				S.add([y+dy, x+dx]);
 			}
 		}
@@ -81,8 +73,9 @@ function part2(data) {
 function main() {
 	let data = fs.readFileSync("15_input").toString("utf-8");
 
-	part1(data);
-	part2(data);
+	// part1(data);
+	part2(data, 1);
+	part2(data, 2);
 	process.exit(0);
 }
 
