7b34da4eb32558a63748d7c24900417fa7f27ab2
Piero <piero.anic@gmail.com>
2021-12-17 15:50:53 +0100

    + optimize for bigboy

--- 2021/17.js
+++ 2021/17.js
@@ -11,11 +11,16 @@ const { min, max, random, abs, ceil, floor, log, log10, log2, round, sign, sin, 
 const { isSuperset, or, and, xor, sub } = set;
 // const { parsePacket, ops, HEX } = require('./lib_2021'); // specific to AOC 2021
 
-const AREA = [[96,125], [-144,-98]];
-// const AREA = [[20,30], [-10,-5]]
+// const AREA = [[96,125], [-144,-98]];
+const AREA = [[2000, 2039], [-9960, -9956]];
+// 49595820 
+// 336 
+// const AREA = [[22000, 22045], [-99960, -99956]];
+// 4995950820 
+// 391 
 
 const simulate = (vx, vy) => {
-	let h = 0, px = 0, py = 0, wasIn = false;
+	let px = 0, py = 0, wasIn = false;
 	let dy = -1, dx = -1
 	while (px < AREA[0][1] && py > AREA[1][0]) {
 		px += vx;
@@ -23,13 +28,12 @@ const simulate = (vx, vy) => {
 		vx += dx;
 		--vy;
 		if (!vx) dx = 0;
-		h = max(h, py);
 		if ( AREA[0][0] <= px && px <= AREA[0][1] 
 			&& AREA[1][0] <= py && py <= AREA[1][1]) {
-			wasIn = true;
+			return 1;
 		}
 	}
-	return [h, px, py, wasIn];
+	return 0;
 }
 
 function part1(data) {
@@ -37,28 +41,44 @@ function part1(data) {
 	let res1 = 0, res2 = 0;
 
 	const S = n => n * (n + 1) / 2;
-	const X = x => -1/2 + sqrt(2*x + 1/4);
-	const mnx = ceil(X(AREA[0][0]));
-	const mxx = floor(X(AREA[0][1]));
 
 	res1 = S(-1 * (min(...AREA[1]) + 1));
+	const xs = [], ys = [];
+	for (let x = 0; x <= AREA[0][1]; ++x) {
+		let dx = x;
+		let rx = 0;
+		while (dx) {
+			rx += dx;
+			if (AREA[0][0] <= rx && rx <= AREA[0][1]) {
+				xs.push(x);
+				break;
+			}
+			--dx;
+		}
+	}
 
-	range(-1000, 1000)(vy => {
-		range(0, 1000)(vx => {
-			const [h, px, py, wasIn] = simulate(vx, vy);
-			/* bruteforce p1 */
-			// if (wasIn && h > res1) {
-			// 	res1 = h;
-			// }
-			res2 += Number(wasIn)
+	for (let y = AREA[1][0]; y <= -AREA[1][0]; ++y) {
+		let dy = y;
+		let ry = 0;
+		while (dy >= AREA[1][0]) {
+			ry += dy;
+			if (AREA[1][0] <= ry && ry <= AREA[1][1]) {
+				ys.push(y);
+				break;
+			}
+			--dy;
+		}
+	}
+
+	iter(xs, x => {
+		iter(ys, y => {
+			res2 += simulate(x, y)
 		})
-	});
+	})
 
 	debug(res1);
-	// exec(`echo ${res} | xclip -sel clip -rmlastnl`);
 	console.log("END OF PART1");
 	debug(res2);
-	// exec(`echo ${res} | xclip -sel clip -rmlastnl`);
 	console.log("END OF PART2");
 
 	return;
