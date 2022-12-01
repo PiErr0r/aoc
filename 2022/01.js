f640b8eb529956e79158336dbb7f4527618da1e4
Piero <piero.anic@gmail.com>
2022-12-01 06:42:29 +0100

    + day 1 @4min

---
+++ 2022/01.js
@@ -0,0 +1,46 @@
+
+const fs = require('fs');
+const { exec } = require("child_process");
+const { bin, float, hex, int, num, oct } = require("../lib");
+const { range, drange, trange, iter, diter, titer } = require("../lib");
+const { copy,	entries, in_, inBB, keys, sort, values } = require("../lib");
+const { Counter, DD, empty, FastQueue, PriorityQueue, Queue, set, Stack } = require("../lib");
+const { ord, chr, count, debug, disp, crt, gcd, lcm, modPow, mod, prod, prodBig, randint, sum, sumBig, transpose } = require("../lib");
+const { digits, ints,	floats,	singles,	words,	lines,	table,	groups,	getGroups,	groupsWith,	parse,	parseLine } = require ('../lib');
+const { min, max, random, abs, ceil, floor, log, log10, log2, round, sign, sin, cos, tan, asin, acos, atan, atan2, sqrt, PI } = Math;
+const { isSuperset, or, and, xor, sub } = set;
+const { getExecStr } = require("../lib/post");
+const { combinations, combinations_with_replacement, next_permutation, product } = require("../lib/itertools");
+// const { parsePacket, ops, HEX, ALU, VARS, resetVars } = require('./lib_2021'); // specific to AOC 2021
+
+function part1(data) {
+
+	data = groups(data).map(g => sum(g.map(int)));
+	let res = max(...data);
+
+	debug(res);
+	exec(`echo ${res} | xclip -sel clip -rmlastnl`);
+	console.log("END OF PART1");
+	return;
+}
+
+function part2(data) {
+	data = groups(data).map(g => sum(g.map(int)))
+	sort(data, (a, b) => b-a)
+	let res = sum(data.slice(0, 3))
+
+	debug(res);
+	// exec(`echo ${res} | xclip -sel clip -rmlastnl`);
+	console.log("END OF PART2");
+	return;
+}
+
+function main() {
+	let data = fs.readFileSync("01_input").toString("utf-8");
+
+	part1(data);
+	part2(data);
+	process.exit(0);
+}
+
+main();
