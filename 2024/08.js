c441e8be9a0b7cb69dc202d3be6048e1080e04bf
piero <piero.anic@gmail.com>
2024-12-08 11:23:11 +0100

    + 2024 day 8

---
+++ 2024/08.js
@@ -0,0 +1,102 @@
+
+const fs = require('fs');
+const { exec } = require("child_process");
+const { ALPHA_L, ALPHA_U, NUMS, D4, D6, D8, MOD } = require("../lib");
+const { joseph, findCycle } = require("../lib");
+const { cache } = require("../lib");
+const { bin, float, hex, int, num, oct } = require("../lib");
+const { range, drange, trange, iter, diter, titer } = require("../lib");
+const { copy,	entries, in_, inBB, keys, sort, values } = require("../lib");
+const { Counter, DD, empty, FastQueue, PriorityQueue, Queue, set, Stack } = require("../lib");
+const { areaInt, circumference, manDist, shoelace } = require("../lib");
+const { ord, chr, count, debug, disp, disp3, crt, gcd, lcm, modPow, modPowBig, modInv, mod, prod, prodBig, randint, sum, sumBig, transpose } = require("../lib");
+const { digits, ints,	floats,	singles,	words,	lines,	table,	groups,	getGroups,	groupsWith,	parse,	parseLine, scanf } = require ('../lib');
+const { min, max, random, abs, ceil, floor, log, log10, log2, round, sign, sin, cos, tan, asin, acos, atan, atan2, sqrt, PI } = Math;
+const { disjoint, isSubset, isSuperset, or, and, xor, sub } = set;
+const { getExecStr } = require("../lib/post");
+const { combinations, combinations_with_replacement, next_permutation, product } = require("../lib");
+
+function part1(data) {
+
+	let res = 0;
+	data = table(data);
+	const antenna = new DD(set);
+
+	iter(data, (r, i) => {
+		iter(r, (c, j) => {
+			if (c !== '.') {
+				antenna[c].add([i, j]);
+			}
+		})
+	})
+
+	const freq = new set();
+	iter(keys(antenna), k => {
+		diter(antenna[k], ([y1, x1], [y2, x2]) => {
+			if (y2 === y1 && x2 === x1) return;
+			const [dy, dx] = [y2 - y1, x2 - x1];
+			if (inBB(y2 + dy, x2 + dx, data)) {
+				freq.add([y2+dy, x2+dx]);
+				data[y2+dy][x2+dx] = '#';
+			}
+		})
+	})
+	res = freq.size
+
+	debug(res);
+	if (res) exec(`echo ${res} | xclip -sel clip -rmlastnl`);
+	console.log("END OF PART1");
+	return;
+}
+
+function part2(data) {
+
+	let res = 0;
+	data = table(data);
+	const antenna = new DD(set);
+
+	iter(data, (r, i) => {
+		iter(r, (c, j) => {
+			if (c !== '.') {
+				antenna[c].add([i, j]);
+			}
+		})
+	})
+
+	const freq = new set();
+	iter(keys(antenna), k => {
+		if (antenna[k].size > 1) {
+			iter(antenna[k], ([y, x]) => {
+				freq.add([y, x]);
+			}) 
+		}
+		diter(antenna[k], ([y1, x1], [y2, x2]) => {
+			if (y2 === y1 && x2 === x1) return;
+			const [ddy, ddx] = [y2 - y1, x2 - x1];
+			let [dy, dx] = [ddy, ddx]
+			while (inBB(y2 + dy, x2 + dx, data)) {
+				freq.add([y2+dy, x2+dx]);
+				if (data[y2+dy][x2+dx] === '.')
+					data[y2+dy][x2+dx] = '#';
+				dy += ddy;
+				dx += ddx;
+			}
+		})
+	})
+	res = freq.size
+
+	debug(res);
+	if (res) exec(`echo ${res} | xclip -sel clip -rmlastnl`);
+	console.log("END OF PART2");
+	return;
+}
+
+function main() {
+	let data = fs.readFileSync("08_input").toString("utf-8");
+
+	part1(data);
+	part2(data);
+	process.exit(0);
+}
+
+main();
